c94d914fccc42a09d583972b72690e30158d0cab
Saket Kolluru <128839034+Saketk2@users.noreply.github.com>
2024-07-22 14:38:36 -0400

    Update signup.js

--- app/screens/signup.js
+++ app/screens/signup.js
@@ -1,19 +1,51 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
+import { View, Text, StyleSheet, TextInput, TouchableOpacity, Alert, Image } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
 
-const LoginScreen = () => {
+const SignupScreen = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [passwordDupe, setPasswordDupe] = useState('');
+  const [username, setUsername] = useState('');
 
-  const handleLogin = () => {
-    // Add your login logic here
-    console.log('Email:', email);
-    console.log('Password:', password);
+  const handleSignup = () => {
+    if (!email || !password || !passwordDupe || !username) {
+      Alert.alert('All fields are required.');
+      return;
+    }
+
+    if (password !== passwordDupe) {
+      Alert.alert('Passwords do not match.');
+      return;
+    }
+    const signupSuccess = true; 
+
+    if (signupSuccess) {
+      Alert.alert('Account created successfully.');
+    } else {
+      Alert.alert('Failed to create account.');
+    }
   };
 
   return (
+    <LinearGradient
+      colors={['#7BE9F1', '#FFFFFF']} 
+      start={{ x: 0, y: 0 }}  
+      end={{ x: 1, y: 1 }}  
+      style={styles.container}
+    >
     <View style={styles.container}>
+      <Image
+        source={require('../images/FinjourneyLogo.png')}
+        style={styles.image}
+      />
       <Text style={styles.title}>Sign up</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Username"
+        value={username}
+        onChangeText={setUsername}
+      />
       <TextInput
         style={styles.input}
         placeholder="Email"
@@ -28,10 +60,18 @@ const LoginScreen = () => {
         onChangeText={setPassword}
         secureTextEntry
       />
-      <TouchableOpacity style={styles.button} onPress={handleLogin}>
+      <TextInput
+        style={styles.input}
+        placeholder="Confirm Password"
+        value={passwordDupe}
+        onChangeText={setPasswordDupe}
+        secureTextEntry
+      />
+      <TouchableOpacity style={styles.button} onPress={handleSignup}>
         <Text style={styles.buttonText}>Let's Go!</Text>
       </TouchableOpacity>
     </View>
+    </LinearGradient>
   );
 };
 
@@ -50,24 +90,27 @@ const styles = StyleSheet.create({
   },
   input: {
     height: 50,
-    borderColor: '#ccc',
-    borderWidth: 1,
+    borderColor: '#OOO',
+    borderWidth: 0.5,
     borderRadius: 5,
     paddingHorizontal: 10,
     marginBottom: 15,
-    width: 250
+    width: 250,
   },
   button: {
     backgroundColor: '#10C1F1',
     paddingVertical: 10,
     borderRadius: 5,
     alignItems: 'center',
-    width: 250
+    width: 250,
   },
   buttonText: {
     color: '#fff',
     fontSize: 16,
   },
+  image: {
+    bottom: 60
+  },
 });
 
-export default LoginScreen;
+export default SignupScreen;
