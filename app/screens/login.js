2e8e6ea4e8ff4e1ad3f0b2bc9168c7e7e6d088ac
Saket Kolluru <128839034+Saketk2@users.noreply.github.com>
2024-07-22 14:37:58 -0400

    Update login.js

--- app/screens/login.js
+++ app/screens/login.js
@@ -1,18 +1,29 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
+import { View, Text, StyleSheet, TextInput, TouchableOpacity, Image } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
 
 const LoginScreen = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
   const handleLogin = () => {
-    // Add your login logic here
-    console.log('Email:', email);
-    console.log('Password:', password);
-  };
+    if (!email || !password) {
+      Alert.alert('All fields are required.');
+      return;
+  };}
     
   return (
+    <LinearGradient
+      colors={['#7BE9F1', '#FFFFFF']} 
+      start={{ x: 0, y: 0 }}  
+      end={{ x: 1, y: 1 }}  
+      style={styles.container}
+    >
     <View style={styles.container}>
+      <Image
+        source={require('../images/FinjourneyLogo.png')}
+        style={styles.image}
+      />
       <Text style={styles.title}>Login</Text>
       <TextInput
         style={styles.input}
@@ -32,6 +43,7 @@ const LoginScreen = () => {
         <Text style={styles.buttonText}>Log in</Text>
       </TouchableOpacity>
     </View>
+    </LinearGradient>
   );
 };
 
@@ -39,6 +51,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
+    alignItems: 'center',
     padding: 20,
   },
   title: {
@@ -49,21 +62,26 @@ const styles = StyleSheet.create({
   },
   input: {
     height: 50,
-    borderColor: '#ccc',
-    borderWidth: 1,
+    borderColor: '#OOO',
+    borderWidth: 0.5,
     borderRadius: 5,
     paddingHorizontal: 10,
     marginBottom: 15,
+    width: 250,
   },
   button: {
     backgroundColor: '#10C1F1',
     paddingVertical: 10,
     borderRadius: 5,
     alignItems: 'center',
+    width: 250,
   },
   buttonText: {
     color: '#fff',
     fontSize: 16,
+  },
+  image: {
+    bottom: 60
   },
 });
 
