63f60e91ac7fc6d25623a154f0d60aa08dcf1efc
Saket Kolluru <saketkolluru.27@gmail.com>
2024-07-29 16:06:46 -0400

    mode and time select + button navigation

--- app/App.js
+++ app/App.js
@@ -2,25 +2,25 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import IntroScreens from './screens/introScreens';
-import LandingScreen from './screens/landingScreen';
-import LoginScreen from './screens/login';
-import SignUpScreen from './screens/signup';
 import GoalInput from './screens/goalInput';
 import GoalIdeas from './screens/goalIdeas';
+import TimeInput from './screens/timeInput';
+import ModeSelector from './screens/modeSelector';
 
 const Stack = createStackNavigator();
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Landing">
+      <Stack.Navigator initialRouteName="Intro">
         <Stack.Screen name="Intro" component={IntroScreens} />
         <Stack.Screen name="GoalInput" component={GoalInput} />
         <Stack.Screen name="GoalIdeas" component={GoalIdeas} />
+        <Stack.Screen name="TimeInput" component={TimeInput} />
+        <Stack.Screen name="ModeSelector" component={ModeSelector} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 };
 
 export default App;
-
