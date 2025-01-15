e71b272aa0fefa5d0c710843d72f30b97a235058
Saket Kolluru <saketkolluru.27@gmail.com>
2025-01-14 20:22:04 -0500

    Created basic UI for feed view

--- app/screens/feed.js
+++ app/screens/feed.js
@@ -1,7 +1,67 @@
-import React, { useRef, useState } from 'react';
+import React, { useState } from 'react';
+import { View, TextInput, TouchableOpacity, Text, StyleSheet } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient'; 
 
 const Feed = () => {
-    return (<h1>Feed Page</h1>)
-}
+    const handleFriends = () => {
+        console.log('You clicked the Friends button');
+    };
+
+    return (
+        <LinearGradient
+        colors={['rgba(16, 193, 241, 0.6)', 'rgba(245, 40, 145, 0.3)']}
+        start={{ x: 0, y: 0 }}  
+        end={{ x: 1, y: 1 }}  
+        style={styles.container}
+        >
+            <View style={styles.innerContainer}>
+                <TouchableOpacity style={styles.button} onPress={handleFriends}>
+                    <Text style={styles.buttonText}>Friends</Text>
+                </TouchableOpacity>
+            </View>
+        </LinearGradient>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    innerContainer: {
+        flex: 1,
+        padding: 20,
+        justifyContent: 'center',
+        borderRadius: 16,
+        margin: 20,
+    },
+    button: {
+        position: 'absolute', // Allows manual positioning
+        top: 10, // Distance from the top
+        left: 10, // Distance from the left
+        backgroundColor: 'rgba(16, 193, 241, 0.8)', // Button background
+        paddingVertical: 15,
+        paddingHorizontal: 30,
+        borderRadius: 10,
+        borderColor: '#fff', // Border color
+        borderWidth: 0.5,
+        // Shadow for iOS
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 4 },
+        shadowOpacity: 0.3,
+        shadowRadius: 4,
+        // Shadow for Android
+        elevation: 5,
+        height: 30,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    buttonText: {
+        color: '#fff',
+        fontSize: 16,
+        fontWeight: 'bold',
+        textAlign: 'center', // Ensures text alignment
+    },
+    
+});
 
 export default Feed;
